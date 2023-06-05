ab6e8b3c325d98f79107ed92e39564ac9f38661f
faarukbulut <faarukbulut@gmail.com>
2023-06-05 09:05:32 +0300

    Article - Category - User > Factory & Seeder

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -3,21 +3,27 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article>
- */
 class ArticleFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
+        $name = fake()->name;
         return [
-            //
+            "category_id" => random_int(1,10),
+            "user_id" => random_int(1,10),
+            "name" => $name,
+            "slug" => Str::slug($name),
+            "body" => fake()->paragraph,
+            "status" => fake()->boolean(),
+            "tags" => Str::slug(fake()->address, ", "),
+            "seo_description" => fake()->paragraph,
+            "seo_keywords" => Str::slug(fake()->address, ", "),
+            "view_count" => random_int(0,100),
+            "like_count" => random_int(0,100),
+            "read_time" => random_int(0,2000),
+            "publish_date" => fake()->dateTime,
         ];
     }
 }
--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -3,21 +3,22 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
- */
 class CategoryFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
+        $name = fake()->name;
         return [
-            //
+            "name" => $name,
+            "slug" => Str::slug($name),
+            "description" => fake()->text,
+            "status" => fake()->boolean,
+            "feature_status" => fake()->boolean,
+            "order" => random_int(0,100),
+            "seo_keywords" => Str::slug(fake()->address, ","),
+            "seo_description" => fake()->paragraph,
         ];
     }
 }
